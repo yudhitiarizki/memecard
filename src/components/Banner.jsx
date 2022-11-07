19737cb7b2aa64246df854466153d2dfa7ae8920
Yudhitia Rizky <yudhitiarizky2002@gmail.com>
2022-11-07 17:11:43 +0700

    update

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,16 +6,10 @@ import 'bootstrap/dist/js/bootstrap.js';
 const Banner = () => {
     return (
         <div className="main-banner">
-            <div className="row">
-                <div className="col-lg-7">
-                    <div className="header-text">
-                        <h6>Welcome To SpartaCodingClub</h6>
-                        <h4><em>Browse</em> Yu-Gi-Oh Cards Meme</h4>
-                        <div className="main-button">
-                            <p>Browse Now</p>
-                        </div>
-                    </div>
-                </div>
+            <h6>Welcome To SpartaCodingClub</h6>
+            <h4><em>Browse</em> Yu-Gi-Oh Cards Meme</h4>
+            <div className="main-button">
+                <p>Browse Now</p>
             </div>
         </div>
     );
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import '../assets/css/style.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
+import { deletecard } from "../redux/modules/cards.js";
 
 const List = ({ children }) => {
+  const dispatch = useDispatch();
   const cards = useSelector((state) => state.cards.cards);
+
+  const onDeletecard = (id) => {
+    dispatch(deletecard(id));
+  };
+
+  
   return (
     <div className="cards-container">
         <div className="row">
@@ -24,6 +32,9 @@ const List = ({ children }) => {
                         </div>
                         <div className="col-12 item-button">
                             <Link className="button-main" to={`/${card.id}`}>Detail</Link>
+                        </div>
+                        <div className="col-12 item-button">
+                            <button className="button-main hapus" type="button" onClick={() => onDeletecard(card.id)}>Hapus</button>
                         </div>
                         </div>
                     </div>
