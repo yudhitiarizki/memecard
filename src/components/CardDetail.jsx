93f54f74ce1503f662b780c3199b28a00ca75547
annisana12 <98462398+annisana12@users.noreply.github.com>
2022-11-08 00:23:50 +0700

    Add back button

--- src/components/CardDetail.jsx
+++ src/components/CardDetail.jsx
@@ -5,29 +5,37 @@ import 'bootstrap/dist/js/bootstrap.js';
 
 const CardDetail = () => {
     return (
-        <div className="top_container">
-            <div>
-                <div className="cardbox thumb">
-                    <img src="https://images-cdn.9gag.com/photo/aQX6YeK_700b.jpg" alt="" />
-                </div>
+        <div>
+            <div className="goback">
+                <button className="goback_button">
+                    <i class="fa-solid fa-house"></i>
+                    Go Home
+                </button>
             </div>
-            <div>
+            <div className="top_container">
                 <div>
-
+                    <div className="cardbox thumb">
+                        <img src="https://images-cdn.9gag.com/photo/aQX6YeK_700b.jpg" alt="" />
+                    </div>
                 </div>
-                <div className="newComment">
-                    <h4>ADD NEW COMMENT</h4>
-                    <form>
-                        <div class="input-list">
-                            <label for="name">Name</label>
-                            <input type="text" placeholder="Input your name here" id="name" name="name_input" />
-                        </div>
-                        <div class="input-list">
-                            <label for="comment">Comment</label>
-                            <textarea type="text" placeholder="Add your comment here" id="comment" name="comment_input" rows="5"/>
-                        </div>
-                    </form>
-                    <button className="addcomment_button">Add New Comment</button>
+                <div>
+                    <div>
+
+                    </div>
+                    <div className="newComment">
+                        <h4>ADD NEW COMMENT</h4>
+                        <form>
+                            <div class="input-list">
+                                <label for="name">Name</label>
+                                <input type="text" placeholder="Input your name here" id="name" name="name_input" />
+                            </div>
+                            <div class="input-list">
+                                <label for="comment">Comment</label>
+                                <textarea type="text" placeholder="Add your comment here" id="comment" name="comment_input" rows="5" />
+                            </div>
+                        </form>
+                        <button className="addcomment_button">Add New Comment</button>
+                    </div>
                 </div>
             </div>
         </div>
